a4b8b64cb8ba97c3507d94a4ddce7ef125721b22
Eprhyll <ephryllbitang13@gmail.com>
2024-05-23 00:21:37 +0800

    learning java 🙂🙂🙂

---
+++ Employee.java
@@ -0,0 +1,14 @@
+public class Employee {
+  private String name;
+  private double payRate;
+  private final int EMPLOYEE_ID;
+  private int nextID;
+  public static final double STARTING_PAY_RATE = 400;
+
+  // public Employee (String name) {
+  // this.name = name;
+  // EMPLOYEE_ID;
+  // payRate = STARTING_PAY_RATE;
+  // }
+
+}
---
+++ tryingSomthn.java
@@ -0,0 +1,21 @@
+public class tryingSomthn {
+
+  public static void main(String[] args) {
+
+    // could be
+    String name3 = 55 + "55";
+    System.out.println(name3); // >> 5555
+
+    // could be
+    String a = "2000";
+    String b = "400";
+    System.out.println(Integer.parseInt(a) + b); // >> 2000400
+
+    int j = 12;
+    double k = 15.0;
+
+    // System.out.println(int(k) + j);
+
+    System.out.println("reality" + 40 + 50 + 0); // >> reality40500
+  }
+}
